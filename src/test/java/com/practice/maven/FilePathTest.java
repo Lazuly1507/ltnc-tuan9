caeba8897551f18e26979d7485fd4e92927169eb
Lazuly <nhlazuly@gmail.com>
2026-05-07 16:49:09 +0700

    b4

--- src/test/java/com/practice/maven/FilePathTest.java
+++ src/test/java/com/practice/maven/FilePathTest.java
@@ -3,11 +3,14 @@ package com.practice.maven;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FilePathTest {
   @Test
   void testWorksEverywhere() {
-    // Dùng Paths.get để tự thích nghi với mọi OS
-    java.nio.file.Path path = java.nio.file.Paths.get("pom.xml");
-    assertTrue(java.nio.file.Files.exists(path));
+    Path path = Paths.get("pom.xml");
+    assertTrue(Files.exists(path));
   }
 }
