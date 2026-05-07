3c2171ddd7cea565a0eb6ce3c0d8c41ded4ecab1
Lazuly <nhlazuly@gmail.com>
2026-05-07 17:11:37 +0700

    b7

--- src/main/java/com/practice/maven/MathUtils.java
+++ src/main/java/com/practice/maven/MathUtils.java
@@ -5,7 +5,8 @@ import org.slf4j.LoggerFactory;
 
 public class MathUtils {
     private static final Logger logger = LoggerFactory.getLogger(MathUtils.class);
-    
+    int My_Variable = 1;
+
     public int add(int a, int b) {
         logger.info("Đang thực hiện phép cộng: {} + {}", a, b);
         return a + b;
